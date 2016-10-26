6da4d75c3838efdfb28b2acd8d5311ca719a9e98
QbP <guille22ce@gmail.com>
2016-10-27 00:24:27 +0100

    Arreglado el bucle

--- src/metodo/de/ordenacion/Ordenar1Vector.java
+++ src/metodo/de/ordenacion/Ordenar1Vector.java
@@ -1,13 +1,13 @@
 public class Ordenar1Vector implements OrdenarVector {
     public void ordena (int[] vec, DatosEstadisticos de){
         int[] nuevo = new int[vec.length];
-        nuevo[0] = vec[0];
-        for(int i = 1; i < vec.length; i++){
+        nuevo[1] = vec[1];
+        for(int i = 2; i < vec.length; i++){
             int j = 0;
             while((j < i) && (nuevo[j] <= vec[i])){
                 j += 1;
             }
-            for(int k = i-1; k < j; k--){
+            for(int k = i-1; k < j; k++){
                 nuevo[k+1] = nuevo[k];
             }
             nuevo[j] = vec[i];
