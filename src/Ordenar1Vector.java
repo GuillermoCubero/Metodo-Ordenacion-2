40413447cad1d535d05b00f2c602cf66df6f1e24
QbP <guille22ce@gmail.com>
2016-11-08 23:47:55 +0000

    Compara ambos metodos

--- src/Ordenar1Vector.java
+++ src/Ordenar1Vector.java
@@ -3,15 +3,19 @@ public class Ordenar1Vector implements OrdenarVector {
         long start = System.currentTimeMillis();
         int[] nuevo = new int[vec.length];
         nuevo[0] = vec[0];
+        de.añadeMovimiento();
         for(int i = 1; i < vec.length; i++){
             int j = 0;
             while((j < i) && (nuevo[j] <= vec[i])){
+                de.añadeComparacion();
                 j += 1;
             }
             for(int k = i-1; k >= j; k--){
                 nuevo[k+1] = nuevo[k];
+                de.añadeMovimiento();
             }
             nuevo[j] = vec[i];
+            de.añadeMovimiento();
         }
         de.añadeTiempo((float) (System.currentTimeMillis() - start));
         System.arraycopy(nuevo, 0, vec, 0, vec.length);
